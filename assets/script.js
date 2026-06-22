3fd078577e6038c83440cc731e8a5ed6c35380e8
mateusveck <mateusbrockveldalvs@gmail.com>
2026-06-22 11:15:48 -0300

    adicionando item na tela

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,31 @@ const category = document.querySelector("#category")
 const price = document.querySelector("#price")
 const button = document.querySelector("#button")
 const list = document.querySelector("#list")
+const template = document.querySelector("#expense-template")
+const finalResult = document.querySelector(".final-result")
+
+const categories = {
+  alimentacao: {
+    name: "Alimentação",
+    icon: "assets/imgs/icons/Food.svg"
+  },
+  hospedagem: {
+    name: "Hospedagem",
+    icon: "assets/imgs/icons/Bed.svg"
+  },
+  transporte: {
+    name: "Transporte",
+    icon: "assets/imgs/icons/Car.svg"
+  },
+  servicos: {
+    name: "Serviços",
+    icon: "assets/imgs/icons/Receipt.svg"
+  },
+  outros: {
+    name: "Outros",
+    icon: "assets/imgs/icons/Wrench.svg"
+  }
+}
 
 function checkForm() {
   if (category.selectedIndex === 0) {
@@ -16,6 +41,34 @@ category.addEventListener("change", checkForm)
 
 let expenses = []
 
+function renderExpenses() {
+  list.innerHTML = ""
+
+  expenses.forEach((despesa) => {
+    const item = template.content.cloneNode(true)
+
+    const categoryInfo = categories[despesa.category]
+
+    item.querySelector(".expense-title").textContent = despesa.title
+    item.querySelector(".expense-category").textContent = categoryInfo.name
+
+    item.querySelector(".expense-icon").src = categoryInfo.icon
+    item.querySelector(".expense-icon").alt = categoryInfo.name
+
+    item.querySelector(".expense-price").textContent = despesa.price.toLocaleString("pt-BR", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })
+
+    item.querySelector(".expense-delete").addEventListener("click", () => {
+      expenses = expenses.filter((expense) => expense.id !== despesa.id)
+
+      renderExpenses()
+    })
+
+    list.append(item)
+  })
+}
 
 form.addEventListener('submit' , (event) =>{
     event.preventDefault()
@@ -41,6 +94,7 @@ if (Number.isNaN(expensePrice) || expensePrice <= 0) {
 }
 
 let despesa = {
+    id: Date.now(),
     title: expenseTitle,
     category: expenseCategory,
     price: expensePrice,
@@ -48,6 +102,7 @@ let despesa = {
 }
 
 expenses.push(despesa)
+renderExpenses()
 form.reset()
 checkForm()
 
